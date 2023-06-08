f4409438a9b364cbdc8270a78510bda22a0f67bb
AbhijeetBijwe123 <abhijeetbijwe94@gmail.com>
2023-06-08 16:41:04 +0530

    No conflicat bcoz we have pull updated code

--- src/com/multiplication/demo/Multiply.java
+++ src/com/multiplication/demo/Multiply.java
@@ -10,6 +10,9 @@ public class Multiply {
 		// added from github
 		a=50;b=60;
 		System.out.println("Multiplication : "+a*b);
+		// added  from local machine
+		a=70;b=80;
+		System.out.println("Multiplication : "+a*b);
 		
 
 	}
