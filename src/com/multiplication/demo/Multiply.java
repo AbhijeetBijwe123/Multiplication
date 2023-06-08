8e7f723893193437be6ea8b91ec69064f725ce4b
AbhijeetBijwe123 <abhijeetbijwe94@gmail.com>
2023-06-08 16:50:53 +0530

    no conflict we added code again

--- src/com/multiplication/demo/Multiply.java
+++ src/com/multiplication/demo/Multiply.java
@@ -3,20 +3,28 @@ package com.multiplication.demo;
 public class Multiply {
 
 	public static void main(String[] args) {
-		int a=10;int b=10;
-		System.out.println("Multiplication : "+a*b);
-		a=30;b=40;
-		System.out.println("Multiplication : "+a*b);
+		int a = 10;
+		int b = 10;
+		System.out.println("Multiplication : " + a * b);
+		a = 30;
+		b = 40;
+		System.out.println("Multiplication : " + a * b);
 		// added from github
-		a=50;b=60;
-		System.out.println("Multiplication : "+a*b);
-		// added  from local machine
-		a=70;b=80;
-		System.out.println("Multiplication : "+a*b);
+		a = 50;
+		b = 60;
+		System.out.println("Multiplication : " + a * b);
+		// added from local machine
+		a = 70;
+		b = 80;
+		System.out.println("Multiplication : " + a * b);
 		// added from git hub
-		a=80;b=90;
-		System.out.println("Multiplication : "+a*b);
-		
+		a = 80;
+		b = 90;
+		System.out.println("Multiplication : " + a * b);
+		// added from local machine again
+		a = 100;
+		b = 110;
+		System.out.println("Multiplication : " + a * b);
 
 	}
 
