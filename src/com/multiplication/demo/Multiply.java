1f8fa7fa1fbb4e04040248d6986d6e5b0fd25da8
AbhijeetBijwe123 <abhijeetbijwe94@gmail.com>
2023-06-08 16:22:30 +0530

    first commit from multiplication

---
+++ src/com/multiplication/demo/Multiply.java
@@ -0,0 +1,11 @@
+package com.multiplication.demo;
+
+public class Multiply {
+
+	public static void main(String[] args) {
+		int a=10;int b=10;
+		System.out.println("Multiplication : "+a*b);
+
+	}
+
+}
