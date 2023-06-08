9831d3bb63705ccc68d5fde7c78c6ef4f4879072
AbhijeetBijwe123 <abhijeetbijwe94@gmail.com>
2023-06-08 16:28:08 +0530

    2 Lines Add from eclipse

--- src/com/multiplication/demo/Multiply.java
+++ src/com/multiplication/demo/Multiply.java
@@ -5,6 +5,8 @@ public class Multiply {
 	public static void main(String[] args) {
 		int a=10;int b=10;
 		System.out.println("Multiplication : "+a*b);
+		a=30;b=40;
+		System.out.println("Multiplication : "+a*b);
 
 	}
 
